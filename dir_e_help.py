e3549d4728331eb72b5f0abeb4688e122cf867f3
hebertzero <hebertzero0@outlook.com>
2020-02-28 11:37:10 -0300

    Create dir_e_help.py

---
+++ dir_e_help.py
@@ -0,0 +1,13 @@
+"""
+Utilitários Python para que auxiliar na programação
+Dir -> Apresenta todos  os atributos e funções/métodos disponíveis
+para determinado tipo de dado ou variável
+dir(tipo de dado/variável)
+
+
+Help-> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável
+help(tipo de dado/variavel.propriedade)
+
+"""
+print(dir('geek'))
