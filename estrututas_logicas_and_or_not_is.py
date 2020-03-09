f80044f6844544ee2725407498be4692ba8714d5
hebertzero <hebertzero0@outlook.com>
2020-03-09 11:53:46 -0300

    estruturas_logicas

---
+++ estrututas_logicas_and_or_not_is.py
@@ -0,0 +1,32 @@
+"""
+Estruturas Lógicas: and (e), or (ou), not (não), is (é)
+
+Operadores unários:
+    - not;
+Operadores binários:
+    - and, or, is;
+
+Regras de funcionamento:
+
+Para o 'and', ambos os valores precisam ser True;
+Para o 'or', um ou outro valor precisa ser True;
+Para o 'not', o valor do booleano é invertido, ou seja, se for True, vira False, se for False Vira True;ss
+Para o 'is', o valor é comparado com um segundo.
+"""
+ativo = True
+
+logado = False
+
+# if ativo and logado:
+#    print('Bem-vindo usuário!')
+# else:
+#    print('Você precisa ativar sua conta. Por favor, cheque seu e-mail.')
+
+# Se não estivar ativo
+if not ativo:
+    print('Você precisa ativar sua conta. Por favor, cheque seu e-mail.')
+else:
+    print('Bem-vindo usuário!')
+
+# ativo é falso?
+print(ativo is False)
